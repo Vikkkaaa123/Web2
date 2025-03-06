6e8442b6272833690db0b8522bbd0d56603f4875
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-03-06 12:47:08 +0300

    Update form.php

--- lesson3/form.php
+++ lesson3/form.php
@@ -5,7 +5,6 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"  href="style.css">
     <title>Форма</title>
-    <link rel="stylesheet" href="style.css">
 </head>
 <body>
     <form action="index.php" method="POST">
