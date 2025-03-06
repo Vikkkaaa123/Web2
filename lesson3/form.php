a5361b937b98429e5147e37e91defa74e956c180
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-03-06 13:01:58 +0300

    Update form.php

--- lesson3/form.php
+++ lesson3/form.php
@@ -3,14 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-     <style>
-        body {
-            background-color: #f0f8ff; /* Голубой цвет фона */
-        }
-        form {
-            background: #99FFFF; /* Цвет фона формы */
-        }
-    </style>
     <link rel="stylesheet"  href="style.css">
     <title>Форма</title>
 </head>
