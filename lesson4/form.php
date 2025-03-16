ee2025114034db445796566a2a29c82f601f8019
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-03-16 19:09:51 +0300

    Update form.php

--- lesson4/form.php
+++ lesson4/form.php
@@ -13,17 +13,17 @@
         <!-- Поле ФИО -->
         <label for="full_name">ФИО:</label>
         <input type="text" id="full_name" name="full_name" placeholder="Введите Ваше фамилию, имя, отчество" required maxlength="150" value="<?php echo htmlspecialchars($values['full_name']); ?>" <?php if ($errors['full_name']) echo 'class="error"'; ?>>
-        <?php if (!empty($messages['full_name'])) echo $messages['full_name']; ?><br>
+        <?php if (!empty($messages['full_name'])) echo '<div class="error-message">' . $messages['full_name'] . '</div>'; ?><br>
 
         <!-- Поле Телефон -->
         <label for="phone">Телефон:</label>
         <input type="tel" id="phone" name="phone" placeholder="+7" required value="<?php echo htmlspecialchars($values['phone']); ?>" <?php if ($errors['phone']) echo 'class="error"'; ?>>
-        <?php if (!empty($messages['phone'])) echo $messages['phone']; ?><br>
+        <?php if (!empty($messages['phone'])) echo '<div class="error-message">' . $messages['phone'] . '</div>'; ?><br>
 
         <!-- Поле Email -->
         <label for="email">E-mail:</label>
         <input type="email" id="email" name="email" placeholder="Введите Вашу почту" required value="<?php echo htmlspecialchars($values['email']); ?>" <?php if ($errors['email']) echo 'class="error"'; ?>>
-        <?php if (!empty($messages['email'])) echo $messages['email']; ?><br>
+        <?php if (!empty($messages['email'])) echo '<div class="error-message">' . $messages['email'] . '</div>'; ?><br>
 
         <!-- Поле Дата рождения -->
         <label for="birth_date">Дата рождения:</label>
@@ -32,7 +32,7 @@
             <input type="number" id="birth_month" name="birth_month" placeholder="Месяц" min="1" max="12" required value="<?php echo htmlspecialchars($values['birth_month']); ?>" <?php if ($errors['birth_month']) echo 'class="error"'; ?>>
             <input type="number" id="birth_year" name="birth_year" placeholder="Год" min="1900" max="2100" required value="<?php echo htmlspecialchars($values['birth_year']); ?>" <?php if ($errors['birth_year']) echo 'class="error"'; ?>>
         </div>
-        <?php if (!empty($messages['birth_date'])) echo $messages['birth_date']; ?><br>
+        <?php if (!empty($messages['birth_date'])) echo '<div class="error-message">' . $messages['birth_date'] . '</div>'; ?><br>
 
         <!-- Поле Пол -->
         <label>Пол:</label>
@@ -42,7 +42,7 @@
             <input type="radio" id="female" name="gender" value="female" required <?php if ($values['gender'] === 'female') echo 'checked'; ?> <?php if ($errors['gender']) echo 'class="error"'; ?>>
             <label for="female">Женский</label>
         </div>
-        <?php if (!empty($messages['gender'])) echo $messages['gender']; ?><br>
+        <?php if (!empty($messages['gender'])) echo '<div class="error-message">' . $messages['gender'] . '</div>'; ?><br>
 
         <!-- Поле Любимый язык программирования -->
         <label for="languages">Любимый язык программирования:</label>
@@ -51,17 +51,17 @@
                 <option value="<?php echo $id; ?>" <?php if (in_array($id, explode(',', $values['languages']))) echo 'selected'; ?>><?php echo $name; ?></option>
             <?php endforeach; ?>
         </select>
-        <?php if (!empty($messages['languages'])) echo $messages['languages']; ?><br>
+        <?php if (!empty($messages['languages'])) echo '<div class="error-message">' . $messages['languages'] . '</div>'; ?><br>
 
         <!-- Поле Биография -->
         <label for="biography">Биография:</label>
         <textarea id="biography" name="biography" required <?php if ($errors['biography']) echo 'class="error"'; ?>><?php echo htmlspecialchars($values['biography']); ?></textarea>
-        <?php if (!empty($messages['biography'])) echo $messages['biography']; ?><br>
+        <?php if (!empty($messages['biography'])) echo '<div class="error-message">' . $messages['biography'] . '</div>'; ?><br>
 
         <!-- Поле Согласие -->
         <input type="checkbox" id="agreement" name="agreement" required <?php if ($values['agreement']) echo 'checked'; ?> <?php if ($errors['agreement']) echo 'class="error"'; ?>>
         <label for="agreement">С контрактом ознакомлен(а)</label>
-        <?php if (!empty($messages['agreement'])) echo $messages['agreement']; ?><br>
+        <?php if (!empty($messages['agreement'])) echo '<div class="error-message">' . $messages['agreement'] . '</div>'; ?><br>
 
         <input type="submit" value="Сохранить">
     </form>
