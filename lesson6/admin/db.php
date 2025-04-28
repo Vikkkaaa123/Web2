82d95c1a0635f8a70f05ad3e175e6b68aa85bd32
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-04-28 21:41:35 +0300

    Update db.php

--- lesson6/admin/db.php
+++ lesson6/admin/db.php
@@ -1,16 +1,5 @@
 <?php
-session_start();
-header('Content-Type: text/html; charset=UTF-8');
-
-$user = 'u68606';
-$pass = '9347178';
-
-try {
-    $db = new PDO('mysql:host=localhost;dbname=u68606', $user, $pass, [
-        PDO::ATTR_PERSISTENT => true,
-        PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
-    ]);
-} catch (PDOException $e) {
-    die('Ошибка подключения: ' . $e->getMessage());
-}
-?>
+$db = new PDO('mysql:host=localhost;dbname=u68606', 'u68606', '9347178', [
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC
+]);
