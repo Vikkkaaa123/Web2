6a62d2647eb8b1dc7c7bd95483e502047d3af5eb
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-04-29 15:57:59 +0300

    Update test.php

--- lesson6/admin/test.php
+++ lesson6/admin/test.php
@@ -1,15 +1,21 @@
 <?php
 require 'auth.php';
 
+echo "<h2>Данные авторизации:</h2>";
 echo "<pre>";
-var_dump([
-    'PHP_AUTH_USER' => $_SERVER['PHP_AUTH_USER'] ?? 'NOT SET',
-    'PHP_AUTH_PW' => $_SERVER['PHP_AUTH_PW'] ?? 'NOT SET',
-    'DB Status' => 'Connected',
-    'PHP Version' => phpversion()
+print_r([
+    'Полученные данные' => [
+        'Логин' => $login ?? 'Не получен',
+        'Пароль' => $password ? '******' : 'Не получен'
+    ],
+    'Заголовки сервера' => [
+        'PHP_AUTH_USER' => $_SERVER['PHP_AUTH_USER'] ?? 'Отсутствует',
+        'HTTP_AUTHORIZATION' => $_SERVER['HTTP_AUTHORIZATION'] ?? 'Отсутствует',
+        'REDIRECT_HTTP_AUTHORIZATION' => $_SERVER['REDIRECT_HTTP_AUTHORIZATION'] ?? 'Отсутствует'
+    ],
+    'Версия PHP' => phpversion(),
+    'Статус БД' => 'Подключено'
 ]);
 echo "</pre>";
 
-checkAdminAuth();
-
-echo "<h1>Вы успешно авторизованы!</h1>";
+echo "<h1 style='color:green'>Вы успешно авторизованы как: " . htmlspecialchars($login) . "</h1>";
