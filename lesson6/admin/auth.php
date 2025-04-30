11707a98be3949218850f6bba71b23a61bc0dc05
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-04-30 20:13:13 +0300

    Update auth.php

--- lesson6/admin/auth.php
+++ lesson6/admin/auth.php
@@ -2,16 +2,13 @@
 session_start();
 
 function checkAdminAuth() {
-    // Проверяем сессию
     if (!empty($_SESSION['admin'])) {
         return true;
     }
 
-    // Проверяем HTTP Basic Auth
     if (!empty($_SERVER['PHP_AUTH_USER']) && !empty($_SERVER['PHP_AUTH_PW'])) {
         $db = new PDO('mysql:host=localhost;dbname=u68606', 'u68606', '9347178');
-        
-        $stmt = $db->prepare("SELECT * FROM admins WHERE login = ?");
+        $stmt = $db->prepare("SELECT password_hash FROM admins WHERE login = ?");
         $stmt->execute([$_SERVER['PHP_AUTH_USER']]);
         $admin = $stmt->fetch();
         
@@ -21,9 +18,8 @@ function checkAdminAuth() {
         }
     }
 
-    // Если не авторизован
     header('WWW-Authenticate: Basic realm="Admin Panel"');
-    header('HTTP/1.0 401 Unauthorized');
+    header('HTTP/1.1 401 Unauthorized');
     die('Требуется авторизация');
 }
 ?>
