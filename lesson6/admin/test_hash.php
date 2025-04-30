e3e97c7a62cb431f2df4b81687d2e4ddbccd97fb
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-04-30 20:14:53 +0300

    Delete lesson6/admin/test_hash.php

--- lesson6/admin/test_hash.php
+++
@@ -1,7 +0,0 @@
-<?php
-$hash = '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi';
-$password = '123';
-
-echo "Проверка пароля '123': " . (password_verify($password, $hash) ? 'OK' : 'ОШИБКА');
-echo "<br>Информация о хеше: ";
-print_r(password_get_info($hash));
