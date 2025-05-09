b7935959bf0a7ae02f1b03bd834656f58974dd94
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-09 14:17:18 +0300

    Create db.php

---
+++ lesson6/db.php
@@ -0,0 +1,23 @@
+<?php
+function connectDB() {
+    static $db = null;
+
+    if ($db === null) {
+        $user = 'u68606';
+        $pass = '9347178';
+        
+        try {
+            $db = new PDO('mysql:host=localhost;dbname=u68606', $user, $pass, [
+                PDO::ATTR_PERSISTENT => true,
+                PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+                PDO::ATTR_EMULATE_PREPARES => false,
+                PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
+            ]);
+        } catch (PDOException $e) {
+            error_log("Database error: " . $e->getMessage());
+            die("Ошибка подключения к БД. Попробуйте позже.");
+        }
+    }
+    return $db;
+}
+?>
