2f72fecf199213e80f1d1aa34fc4c3648786386a
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-26 18:10:31 +0300

    Update index.php

--- project/index.php
+++ project/index.php
@@ -1,38 +1,28 @@
 <?php
 include('./settings.php');
 
-// Выключаем отображение ошибок после отладки.
 ini_set('display_errors', DISPLAY_ERRORS);
-
-// Папки со скриптами и модулями.
 ini_set('include_path', INCLUDE_PATH);
 
 include('./scripts/db.php');
 include('./scripts/init.php');
 
-$request = array(
-  'url' => isset($_GET['q']) ? $_GET['q'] : '',
-  'method' => isset($_POST['method']) && in_array($_POST['method'], array('get', 'post', 'put', 'delete')) ? $_POST['method'] : $_SERVER['REQUEST_METHOD'],
-  'get' => !empty($_GET) ? $_GET : array(),
-  'post' => !empty($_POST) ? $_POST : array(),
-  'put' => !empty($_POST) && !empty($_POST['method']) && $_POST['method'] == 'put' ? $_POST : array(),
-  'delete' => !empty($_POST) && !empty($_POST['method']) && $_POST['method'] == 'delete' ? $_POST : array(),
-  'Content-Type' => 'text/html',
-);
+$request = [
+  'url' => $_GET['q'] ?? '',
+  'method' => in_array($_POST['method'] ?? '', ['get', 'post', 'put', 'delete']) 
+              ? $_POST['method'] 
+              : strtolower($_SERVER['REQUEST_METHOD']),
+  'get' => $_GET,
+  'post' => $_POST,
+  'put' => ($_POST['method'] ?? '') == 'put' ? $_POST : [],
+  'delete' => ($_POST['method'] ?? '') == 'delete' ? $_POST : [],
+  'Content-Type' => 'text/html'
+];
 
-$response = init($request, $urlconf);
+$response = init($request);
 
-if (!empty($response['headers'])) {
-  foreach ($response['headers'] as $key => $value) {
-    if (is_string($key)) {
-      header(sprintf('%s: %s', $key, $value));
-    }
-    else {
-      header($value);
-    }
-  }
+foreach ($response['headers'] ?? [] as $key => $value) {
+    header(is_string($key) ? "$key: $value" : $value);
 }
 
-if (!empty($response['entity'])) {
-  print($response['entity']);
-}
+echo $response['entity'] ?? '';
