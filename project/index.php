6dc3e8c2405208e9d38fad826c06786215aa6520
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-27 20:04:05 +0300

    Update index.php

--- project/index.php
+++ project/index.php
@@ -6,23 +6,29 @@ ini_set('include_path', INCLUDE_PATH);
 
 include('./scripts/db.php');
 include('./scripts/init.php');
-
-$request = [
-    'url' => $_GET['q'] ?? '',
-    'method' => in_array($_POST['method'] ?? '', ['get', 'post', 'put', 'delete'])
-        ? $_POST['method']
-        : strtolower($_SERVER['REQUEST_METHOD']),
-    'get' => $_GET,
-    'post' => $_POST,
-    'put' => ($_POST['method'] ?? '') == 'put' ? $_POST : [],
-    'delete' => ($_POST['method'] ?? '') == 'delete' ? $_POST : [],
-    'Content-Type' => 'text/html'
-];
+$request = array(
+  'url' => isset($_GET['q']) ? $_GET['q'] : '',
+  'method' => isset($_POST['method']) && in_array($_POST['method'], array('get', 'post', 'put', 'delete')) ? $_POST['method'] : $_SERVER['REQUEST_METHOD'],
+  'get' => !empty($_GET) ? $_GET : array(),
+  'post' => !empty($_POST) ? $_POST : array(),
+  'put' => !empty($_POST) && !empty($_POST['method']) && $_POST['method'] == 'put' ? $_POST : array(),
+  'delete' => !empty($_POST) && !empty($_POST['method']) && $_POST['method'] == 'delete' ? $_POST : array(),
+  'Content-Type' => 'text/html',
+);
 
 $response = init($request, $urlconf);
 
-foreach ($response['headers'] ?? [] as $key => $value) {
-    header(is_string($key) ? "$key: $value" : $value);
+if (!empty($response['headers'])) {
+  foreach ($response['headers'] as $key => $value) {
+    if (is_string($key)) {
+      header(sprintf('%s: %s', $key, $value));
+    }
+    else {
+      header($value);
+    }
+  }
 }
 
-echo $response['entity'] ?? '';
+if (!empty($response['entity'])) {
+  print($response['entity']);
+}
