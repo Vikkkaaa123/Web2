29f0b298b82302963570246f644f095990384cb4
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-29 18:47:54 +0300

    Update login.tpl.php

--- project/theme/login.tpl.php
+++ project/theme/login.tpl.php
@@ -8,20 +8,26 @@
   </head>
   <body>
   <h1>Вход в систему</h1>
-    <form class="login_form" action="<?php echo url('login'); ?>" method="post">
-      <label> 
-        Логин <br/>
-        <input name="login" />
-      </label> <br/>
-      <label> 
-        Пароль <br/>
-        <input name="password" />
-      </label> <br/>
-      
-      <input class="login_button" type="submit" value="Войти" />
 
-      <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
-    </form>
+<?php if (!empty($error)): ?>
+  <div class="error-message" style="color: red; margin-bottom: 10px;">
+    <?= htmlspecialchars($error) ?>
+  </div>
+<?php endif; ?>
+
+<form class="login_form" action="<?php echo url('login'); ?>" method="post">
+  <label> 
+    Логин <br/>
+    <input name="login" />
+  </label> <br/>
+  <label> 
+    Пароль <br/>
+    <input name="password" />
+  </label> <br/>
+  
+  <input class="login_button" type="submit" value="Войти" />
+  <input type="hidden" name="csrf_token" value="<?php echo $csrf_token; ?>">
+</form>
 
   </body>
 </html>
