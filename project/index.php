6cd1d8b5eb0b70458cb5b8740fcc46a58660ea97
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-29 08:19:46 +0300

    Update index.php

--- project/index.php
+++ project/index.php
@@ -10,6 +10,29 @@ ini_set('include_path', INCLUDE_PATH);
 include('./scripts/db.php');
 include('./scripts/init.php');
 
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+// Проверка существования файлов перед включением
+$files_to_include = [
+    './settings.php',
+    './scripts/db.php',
+    './scripts/init.php'
+];
+
+foreach ($files_to_include as $file) {
+    if (!file_exists($file)) {
+        die("Error: Missing required file - $file");
+    }
+    require_once $file;
+}
+
+// Проверка работы db_connect()
+if (!function_exists('db_connect')) {
+    die("Error: db_connect() function not found");
+}
+
 // Определение типа запроса (AJAX или обычный)
 $is_ajax = !empty($_SERVER['HTTP_X_REQUESTED_WITH']) && 
            strtolower($_SERVER['HTTP_X_REQUESTED_WITH']) == 'xmlhttprequest';
