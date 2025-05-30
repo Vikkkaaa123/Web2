f5e7ce064a7cf76a661a150ce1850377ef014c1c
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-30 22:00:48 +0300

    Update logout.php

--- project/modules/logout.php
+++ project/modules/logout.php
@@ -1,27 +1,27 @@
 <?php
 require_once __DIR__ . '/../scripts/db.php';
 
-function logout_post($request) {
-    if (session_status() == PHP_SESSION_NONE) {
-        session_start();
-    }
-    
-    $_SESSION = array();
-    
-    if (ini_get("session.use_cookies")) {
-        $params = session_get_cookie_params();
-        setcookie(
-            session_name(),
-            '',
-            time() - 42000,
-            $params["path"],
-            $params["domain"],
-            $params["secure"],
-            $params["httponly"]
-        );
-    }
-    
-    session_destroy();
-    
-    return redirect('');
+if (session_status() == PHP_SESSION_NONE) {
+    session_start();
 }
+
+$_SESSION = [];
+
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(
+        session_name(),
+        '',
+        time() - 42000,
+        $params["path"],
+        $params["domain"],
+        $params["secure"],
+        $params["httponly"]
+    );
+}
+
+session_destroy();
+
+// Перенаправление на главную
+header('Location: /');
+exit;
