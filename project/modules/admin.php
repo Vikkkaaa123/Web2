d383e7aec9408a08f313f42bd9fd248fb72beb92
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-05-31 15:53:49 +0300

    Update admin.php

--- project/modules/admin.php
+++ project/modules/admin.php
@@ -2,10 +2,10 @@
 require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/../scripts/db.php';
 
-$db = db_connect();
 checkAdminAuth();
 
-function admin_get($db) {
+function admin_get() {
+    $db = db_connect();
     // Статистика по языкам
     $stats = $db->query("
         SELECT p.name, COUNT(DISTINCT al.application_id) as count
