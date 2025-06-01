9117c311dcd9f6081ffc0622cc400afe28fcb988
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-06-01 10:11:01 +0300

    Update admin.php

--- project/modules/admin.php
+++ project/modules/admin.php
@@ -1,9 +1,5 @@
 <?php
-ob_start();
-error_reporting(E_ALL);
-ini_set('display_errors', 1);
-
-require_once __DIR__ . '/auth.php';
+//require_once __DIR__ . '/auth.php';
 require_once __DIR__ . '/../scripts/db.php';
 
 checkAdminAuth();
