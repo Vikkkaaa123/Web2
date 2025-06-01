08f534a3b2b9de49d094ea98d37a26d36c063ebf
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-06-01 10:16:20 +0300

    Update admin.tpl.php

--- project/theme/admin.tpl.php
+++ project/theme/admin.tpl.php
@@ -46,7 +46,7 @@
                 </tr>
             </thead>
             <tbody>
-                <?php foreach ($processedApplications as $app): ?>
+                <?php foreach ($users as $app): ?>
                 <tr>
                     <td><?= $app['id'] ?></td>
                     <td><?= htmlspecialchars($app['user_login'] ?? 'N/A') ?></td>
