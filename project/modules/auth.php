94c390916c3632af8f91d652c55d22968dc51c8b
Vikkkaaa123 <pereuznikviktoria@gmail.com>
2025-06-01 20:42:31 +0300

    Update auth.php

--- project/modules/auth.php
+++ project/modules/auth.php
@@ -3,7 +3,6 @@ if (session_status() === PHP_SESSION_NONE) {
     session_start();
 }
 
-
 function checkAdminAuth() {
     if (empty($_SESSION['login']) || empty($_SESSION['admin']) || $_SESSION['admin'] !== true) {
         // Не админ — редирект на логин
